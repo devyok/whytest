923b97447af70d67d9ae1feb3708f8f987537c19
dengw <hacker686>
2024-08-02 21:07:38 +0800

    2

---
+++ pcore/src/main/java/com/test/pcore/PCore.java
@@ -0,0 +1,10 @@
+package com.test.pcore;
+
+import android.util.Log;
+
+public class PCore {
+    private static final String TAG = "pcore";
+    public static void main(String[] args) {
+        Log.i(TAG,"xxxxx");
+    }
+}
